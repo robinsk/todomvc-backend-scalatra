9f9a3d512db5a3ca7613a612fe69fc0e9c9cc09b
Robin Skoglund <rsk@knowit.no>
2014-05-20 12:00:55 +0200

    appkode

--- src/main/scala/no/knowit/swr/TodoMvcApp.scala
+++ src/main/scala/no/knowit/swr/TodoMvcApp.scala
@@ -1,14 +1,35 @@
-package org.scalatra.example
+package no.knowit.swr
 
 import org.scalatra._
+import org.json4s.{DefaultFormats, Formats}
+import no.knowit.swr.model.{Store, Todo}
 
-class TodoMvcApp extends ScalatraServlet {
+class TodoMvcApp extends ScalatraServlet with JacksonJsonSupport {
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
 
   get("/") {
-    <html>
-      <body>
-        <h1>backend</h1>
-      </body>
-    </html>
+    Store.getAll
   }
+
+  post("/") {
+    val todo = parsedBody.extract[Todo]
+    Store.save(todo)
+  }
+
+  put("/:id") {
+    val id = params("id").toLong
+    val todo = parsedBody.extract[Todo]
+    Store.save(id, todo)
+  }
+
+  delete("/:id") {
+    val id = params("id").toLong
+    Store.remove(id)
+  }
+  
 }
